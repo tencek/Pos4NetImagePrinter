8bd3e16f4330780027b6d77a891ff8076fb3e14e
Pavel Kučera <pavel.kucera@edhouse.cz>
2019-09-27 23:29:56 +0200

    + base func

--- src/Pos4NetImagePrinter/Pos4NetImagePrinter.fs
+++ src/Pos4NetImagePrinter/Pos4NetImagePrinter.fs
@@ -1,78 +1,40 @@
 ﻿open System
 open System.Text
+open Microsoft.PointOfService
 
-#if NETCOREAPP3_0
-let uintToString (value: uint32) =
-    Rune(ch).ToString()
-let validValues (codePage: int) =
-    let cp = Encoding.GetEncoding(codePage)
-    let runes =
-        seq { for u in UInt32.MinValue .. UInt32.MaxValue do if Rune.IsValid(u) then yield Rune(u) }
-    let isCp r =
-        let backRune =
-            r.ToString()
-            |> cp.GetBytes
-            |> cp.GetString
-            |> fun s -> Rune.GetRuneAt(s, 0)
-        r = backRune
-    seq { for r in runes do if isCp r then yield r }
-    |> Seq.map (fun r -> 
-        r.Value
-        |> uint32
-        )
-#else
-let inline isBmp (value: uint32) =
-    value <= 0xFFFFu
-let uintToString (value: uint32) =
-    if isBmp value then
-        (char value).ToString()
-    else
-        let highSurrogateCodePoint = char ((value + ((0xD800u - 0x40u) <<< 10)) >>> 10)
-        let lowSurrogateCodePoint = char ((value &&& 0x3FFu) + 0xDC00u)
-        sprintf "%c%c" highSurrogateCodePoint lowSurrogateCodePoint
 
-let validValues (codePage: int) =
-    let cp = Encoding.GetEncoding(codePage)
-    let inline isValidUnicodeScalar (value: uint32) =
-        ((value - 0x110000u) ^^^ 0xD800u) >= 0xFFEF0800u
-    let validValues =
-        seq { for u in UInt32.MinValue .. UInt32.MaxValue do if isValidUnicodeScalar u then yield u }
-    let isCp v =
-        let backRune =
-            let isHighSurrogateCodePoint (value: uint32) =
-                ((value - 0xD800u) <= (0xDBFFu - 0xD800u))
+let logFunc str =
+   printfn "%s" str
 
-            uintToString v
-            |> cp.GetBytes
-            |> cp.GetString
-            |> fun s -> 
-                if (isHighSurrogateCodePoint (uint32 s.[0])) then
-                    ((uint32 s.[0]) <<< 10) + (uint32 s.[1]) - ((0xD800u <<< 10) + 0xDC00u - (1u <<< 16))
-                else
-                    (uint32 s.[0])
-        v = backRune
-    seq { for v in validValues do if isCp v then yield v }
-#endif
+let errorHandler error =
+   logFunc <| "Error!"
+   logFunc <| error.ToString()
+   System.Console.Beep()
 
+let deviceErrorHandler (eventArgs:DeviceErrorEventArgs) =
+   errorHandler eventArgs
+   System.Console.Beep()
 
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello!"
-    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance)
+
+    let printer = PosPrinter.getReady "Microsoft PosPrinter Simulator"
     
-    let chs = validValues 950
+    printer.ErrorEvent.Add errorHandler
 
-    let lineDisplay = LineDisplayProcessor.getReady "RSLD.EASTASIA.COM1" 950  //"59772000"
+    //if printer.CapRecBitmap then
+    try
+       printer.PrintNormal(PrinterStation.Receipt, "Ejchuchu")
+       printer.PrintBitmap(PrinterStation.Receipt, @"C:\Users\Manžel\Pictures\SmileFace.jpg", 100, PosPrinter.PrinterBitmapCenter)
+    with 
+      ex ->
+         errorHandler ex
+    //else
+      //()
 
-    for ch in chs do
-        let r = uintToString ch
-        let tenChars = sprintf "%s%s%s%s%s%s%s%s%s%s" r r r r r r r r r r
-        let position, pos2 = LineDisplayProcessor.testString lineDisplay tenChars
-        if position <> 20 || pos2 <> 21 then
-            printfn "%i %i 12345678901234567890" position pos2
-            printfn "%i %i %s : %d %04X" position pos2 tenChars ch ch
+    System.Threading.Thread.Sleep 1000
 
-    lineDisplay.Release()
+    printer.Release()
 
     0 // return an integer exit code
--- src/Pos4NetImagePrinter/PosPrinter.fs
+++ src/Pos4NetImagePrinter/PosPrinter.fs
@@ -1,56 +1,38 @@
-module LineDisplayProcessor
+module PosPrinter
 
 open Microsoft.PointOfService
 
-let setUpDevice logicalDeviceName =
+let private setUpDevice deviceName =
     let posExplorer = PosExplorer()
-    let lineDisplayInfoMaybe = posExplorer.GetDevice("LineDisplay", logicalDeviceName)
-    if isNull lineDisplayInfoMaybe then
-        seq { for d in posExplorer.GetDevices("LineDisplay") -> d }
-        |> Seq.tryFind (fun d -> d.ServiceObjectName = logicalDeviceName)
+    let posPrinterInfoMaybe = posExplorer.GetDevice("PosPrinter", deviceName)
+    if isNull posPrinterInfoMaybe then
+        seq { for d in posExplorer.GetDevices("PosPrinter") -> d }
+        |> Seq.tryFind (fun d -> d.ServiceObjectName = deviceName)
     else
-        Some lineDisplayInfoMaybe
-    |> Option.bind (fun lineDisplayInfo ->
+        Some posPrinterInfoMaybe
+    |> Option.bind (fun posPrinterInfo ->
         try
-            let device = posExplorer.CreateInstance(lineDisplayInfo)
-            let lineDisplay = device :?> LineDisplay
-            lineDisplay.Open()
-            Some lineDisplay
+            let device = posExplorer.CreateInstance(posPrinterInfo)
+            let posPrinter = device :?> PosPrinter
+            posPrinter.Open()
+            Some posPrinter
         with ex ->
             printfn "Problem: %A" ex
             None)
-let rec ensure func =
+
+let rec private ensure func =
     match func() with
     | Some v ->
         v
     | None ->
         System.Threading.Thread.Sleep 500
-        printfn "Problem getting a line display"
+        printfn "Problem getting a device"
         ensure func
         
-let getReady logicalDeviceName codePage =
-    let lineDisplay = ensure (fun () -> setUpDevice logicalDeviceName)
-    lineDisplay.Claim(-1)
-    lineDisplay.DeviceEnabled <- true
-    if lineDisplay.CharacterSetList |> Array.contains codePage then
-        printfn "Setting character set: %i" codePage
-        lineDisplay.CharacterSet <- codePage
-        printfn "New current character set: %i" lineDisplay.CharacterSet
-    else
-        printfn "LineDisplay doesn't support code page: %i" codePage
-    if lineDisplay.CapMapCharacterSet then
-        printfn "LineDisplay supports CapMapCharacterSet, using it"
-        lineDisplay.MapCharacterSet <- true
-    else
-        printfn "LineDisplay doesn't support CapMapCharacterSet, we don't have support for converting ourselves, yet"
-    lineDisplay.CursorUpdate <- true
-    lineDisplay
+let getReady logicalDeviceName =
+    let posPrinter = ensure (fun () -> setUpDevice logicalDeviceName)
+    posPrinter.Claim(-1)
+    posPrinter.DeviceEnabled <- true
+    posPrinter
 
-let testString (lineDisplay: LineDisplay) value =
-    lineDisplay.CursorColumn <- 0
-    lineDisplay.DisplayTextAt(0, 0, value)
-    let c =lineDisplay.CursorColumn
-    lineDisplay.DisplayText("|")
-    let c2 =lineDisplay.CursorColumn
-    (c, c2)
     
