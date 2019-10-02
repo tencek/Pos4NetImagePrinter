0c207b2337ad757626f89e0ea6b8c85bfecd1468
Pavel Kučera <pavel.kucera@edhouse.cz>
2019-10-02 12:02:57 +0200

    script improved a bit

--- scripting/ImagePrinting.ps1
+++ scripting/ImagePrinting.ps1
@@ -1,5 +1,6 @@
-﻿$files =  Get-ChildItem .\bmpsuite-2.5\g\
+﻿cd $PSScriptRoot
+$files =  Get-ChildItem bmpsuite-2.5\g
 foreach ($file in $files) 
 {
-   .\Pos4NetImagePrinter.exe /printer TM-T88VMU /path $file.FullName /label $file.Name /Width AsIs /NoCut
+   ..\Release\net472\publish\Pos4NetImagePrinter.exe /printer TM-T88VMU /path $file.FullName /label $file.Name /Width AsIs /NoCut
 }
