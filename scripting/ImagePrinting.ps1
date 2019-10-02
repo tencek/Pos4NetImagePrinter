df762030f13f2e425a0d8496f6558b34325d5194
Pavel Kučera <pavel.kucera@edhouse.cz>
2019-10-02 11:45:54 +0200

    + scripting

---
+++ scripting/ImagePrinting.ps1
@@ -0,0 +1,5 @@
+﻿$files =  Get-ChildItem .\bmpsuite-2.5\g\
+foreach ($file in $files) 
+{
+   .\Pos4NetImagePrinter.exe /printer TM-T88VMU /path $file.FullName /label $file.Name /Width AsIs /NoCut
+}
